f2a9191b40c88d975dbc33ad3e3801862adc4251
Russell O'Connor <roconnor@theorem.ca>
2011-07-05 00:32:33 +0200

    data types for TxInput and TxOutput
    Ignore-this: 44dd16892b6c8b1b1c9e7513cad6e79b
    
    darcs-hash:20110704223233-e9b40-19cea3f9db6bf52e9459355cf64fe7b0e515ea1f

--- Purecoin/Core/DataTypes.hs
+++ Purecoin/Core/DataTypes.hs
@@ -2,16 +2,25 @@ module Purecoin.Core.DataTypes
        ( Difficulty, target, fromTarget
        , Hash, hash0, hashBS
        , Lock, unlocked, lockBlock, lockTime, lockView, LockView(..)
+       , OutPoint, outPoint, opHash, opIndex
+       , BTC(..)
+       , TxInput, txiPreviousOutput, txiScript, txiFinal
+       , TxOutput, txOutput, txoValue, txoScript
        ) where
 
-import Data.Word (Word32, Word8)
+import Data.Word (Word64, Word32, Word8)
 import Data.Bits (shiftR, shiftL, (.|.), (.&.))
 import Data.List (unfoldr)
-import Data.Monoid (mempty)
-import Data.Serialize (Serialize, get, getWord32le, put, putWord32le, encode)
+import Data.Monoid (Monoid, mempty, mappend)
+import Data.Serialize ( Serialize
+                      , get, getWord32le, getWord64le
+                      , put, putWord32le, putWord64le
+                      , encode )
 import Data.Time (UTCTime)
 import Data.Time.Clock.POSIX (posixSecondsToUTCTime, utcTimeToPOSIXSeconds)
+import Control.Monad (guard)
 import Control.Applicative ((<$>),(<*>))
+import Purecoin.Core.Script (Script)
 import Purecoin.Digest.SHA256 (Hash256, sha256)
 import Purecoin.Utils (showHexByteStringLE)
 import qualified Data.ByteString as BS
@@ -126,3 +135,67 @@ lockView :: Lock -> LockView
 lockView (Lock 0) = Unlocked
 lockView (Lock x) | x < lockBlockMaxValue = LockBlock . toInteger $ x
                   | otherwise             = LockTime . posixSecondsToUTCTime . fromIntegral $ x
+
+{- Ord instance is given because OutPoint are used for Keys in a Map -}
+data OutPoint = OutPoint { opHash :: !Hash
+                         , opIndex :: !Word32
+                         } deriving (Eq, Ord, Show)
+
+instance H.Hashable OutPoint where
+  hash (OutPoint h i) = H.combine (H.hash h) (fromIntegral i)
+
+instance Serialize OutPoint where
+  get = do h <- get
+           i <- getWord32le
+           maybe (fail "get (OutPoint): received coinbase") return (outPoint h i)
+
+  put (OutPoint h i) = put h >> putWord32le i
+
+outPoint :: Hash -> Word32 -> Maybe OutPoint
+outPoint h (-1) | h == hash0 = Nothing
+outPoint h i                 = Just (OutPoint h i)
+
+-- We don't want BTC to be a Num because multiplication of a BTC by another BTC doesn't make sense.
+newtype BTC = Satoshi Integer deriving (Eq, Ord)
+
+instance Monoid BTC where
+  mempty = Satoshi 0
+  (Satoshi x) `mappend` (Satoshi y) = Satoshi $ x + y
+
+instance Show BTC where
+  show (Satoshi x) = coins ++ "." ++ padFilings ++ filings ++ " BTC"
+    where
+      rawString = show x
+      len = length rawString
+      (coins,filings) = splitAt (8 - len) rawString
+      padFilings = replicate (8 - length filings) '0'
+
+data TxInput = TxInput { txiPreviousOutput :: OutPoint
+                       , txiScript :: Script
+                       , txiFinal_ :: Word32
+                       } deriving Show
+
+txiFinal :: TxInput -> Bool
+txiFinal txi = txiFinal_ txi == maxBound
+
+instance Serialize TxInput where
+  get = TxInput <$> get <*> get <*> getWord32le
+
+  put (TxInput p s sq) = put p >> put s >> putWord32le sq
+
+data TxOutput = TxOutput { txoValue_ :: !Word64
+                         , txoScript :: !Script
+                         } deriving Show
+
+txoValue :: TxOutput -> BTC
+txoValue = Satoshi . toInteger . txoValue_
+
+instance Serialize TxOutput where
+  get = TxOutput <$> getWord64le <*> get
+
+  put (TxOutput v s) = putWord64le v >> put s
+
+txOutput :: BTC -> Script -> Maybe TxOutput
+txOutput btc@(Satoshi v) s | v < 0     = fail $ "txOutput: value "++show btc++" too small"
+                           | v <= toInteger (maxBound :: Word64) = return $ TxOutput (fromInteger v) s
+                           | otherwise = fail $ "txOutput: value "++show btc++" too large"
