2b57305f6663b897821ba9180c3a8cf1b206f4ff
Russell O'Connor <roconnor@theorem.ca>
2011-07-08 00:55:30 +0200

    data type for Block.
    Ignore-this: 9e1a337eae6e63b4382db59805347ccb
    
    darcs-hash:20110707225530-e9b40-5a4eff718eb1ab7aad44e8dc7e670e00e06111b0

--- Purecoin/Core/DataTypes.hs
+++ Purecoin/Core/DataTypes.hs
@@ -1,6 +1,6 @@
 module Purecoin.Core.DataTypes
-       ( Difficulty, target, fromTarget
-       , Hash, hash0, hashBS
+       ( Hash, hash0, hashBS, hash, merkleHash
+       , Difficulty, target, fromTarget, hashMeetsTarget
        , Lock, unlocked, lockBlock, lockTime, lockView, LockView(..)
        , OutPoint, outPoint, opHash, opIndex
        , BTC(..)
@@ -8,27 +8,69 @@ module Purecoin.Core.DataTypes
        , TxOutput, txOutput, txoValue, txoScript
        , Tx(..)
        , TxCoinBase, txCoinBase, txcbVersion, txcbExtraNonce, txcbFinal, txcbOut, txcbLock
+       , Block, block, bVersion, bPrevBlock, bMerkle_root, bTimestamp, bBits, bCoinBase, bTxs, bHash
        ) where
 
 import Data.Word (Word64, Word32, Word8)
 import Data.Bits (shiftR, shiftL, (.|.), (.&.))
 import Data.List (unfoldr)
-import Data.NEList (NEList)
+import Data.NEList (NEList(..), (<|))
 import Data.Monoid (Monoid, mempty, mappend)
 import Data.Time (UTCTime)
 import Data.Time.Clock.POSIX (posixSecondsToUTCTime, utcTimeToPOSIXSeconds)
-import Control.Monad (guard)
+import Control.Monad (guard, unless, replicateM)
 import Control.Applicative ((<$>),(<*>))
-import Purecoin.Core.Serialize ( Serialize
+import Purecoin.Core.Serialize ( Serialize, Get
                                , get, getWord32le, getWord64le, getVarInteger, getNEList
                                , put, putWord32le, putWord64le, putVarInteger, putNEList
-                               , encode )
+                               , encode, runPut )
 import Purecoin.Core.Script (Script)
 import Purecoin.Digest.SHA256 (Hash256, sha256)
-import Purecoin.Utils (showHexByteStringLE)
+import Purecoin.Utils (showHexByteStringLE, integerByteStringLE)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BSL
 import qualified Data.Hashable as H
+
+-- the Ord instance makes it useful as a key for a Map
+newtype Hash = Hash Hash256 deriving (Eq, Ord)
+
+instance Serialize Hash where
+  get = Hash <$> get
+
+  put (Hash h) = put h
+
+-- The standard way of displaying a bitcoin hash is in little endian format.
+instance Show Hash where
+  show = showHexByteStringLE . encode
+
+instance H.Hashable Hash where
+  hash (Hash h) = H.hash h
+
+-- Like all crap C programs, the 0 value is copted to have a sepearate special meaning.
+hash0 :: Hash
+hash0 = Hash mempty
+
+-- For some reason in bitcoin hashing is done by two separate rounds of sha256.
+-- It makes hashing slower and shortens the effectiveness of the hash by close a little less than a bit.
+-- I do not know what is gained by this.
+hashBS :: BS.ByteString -> Hash
+hashBS = Hash . round . encode . round
+ where
+   round = sha256 . BSL.fromChunks . (:[])
+
+hash :: (Serialize a) => a -> Hash
+hash = hashBS . encode
+
+merkleHash :: NEList Hash -> Hash
+merkleHash (NENil x) = x
+merkleHash l = merkleHash (go l)
+ where
+  merkle :: Hash -> Hash -> Hash
+  h1 `merkle` h2 = hashBS $ encode h1 `BS.append` encode h2
+  go :: NEList Hash -> NEList Hash
+  go (NENil x) = NENil (x `merkle` x)
+  go (NECons x (NENil y)) = NENil (x `merkle` y)
+  go (NECons x (NECons y l)) = NECons (x `merkle` y) (go l)
 
 -- for historical reasons the 9th bit of the difficulty encoding must always be 0.
 -- note: that even though one target may have multiple represenatations in this compact form,
@@ -68,32 +110,10 @@ fromTarget y | y <= 0     = Difficulty 0
    man :: Word32
    man = fromInteger (shiftR y (8*(fromIntegral exp - 3)))
 
--- the Ord instance makes it useful as a key for a Map
-newtype Hash = Hash Hash256 deriving (Eq, Ord)
-
-instance Serialize Hash where
-  get = Hash <$> get
-
-  put (Hash h) = put h
-
--- The standard way of displaying a bitcoin hash is in little endian format.
-instance Show Hash where
-  show = showHexByteStringLE . encode
-
-instance H.Hashable Hash where
-  hash (Hash h) = H.hash h
-
--- Like all crap C programs, the 0 value is copted to have a sepearate special meaning.
-hash0 :: Hash
-hash0 = Hash mempty
-
--- For some reason in bitcoin hashing is done by two separate rounds of sha256.
--- It makes hashing slower and shortens the effectiveness of the hash by close a little less than a bit.
--- I do not know what is gained by this.
-hashBS :: BS.ByteString -> Hash
-hashBS = Hash . round . encode . round
- where
-   round = sha256 . BSL.fromChunks . (:[])
+hashMeetsTarget :: Hash -> Difficulty -> Bool
+hashMeetsTarget h d = hi <= target d
+  where
+    hi = integerByteStringLE . encode $ h
 
 newtype Lock = Lock Word32 deriving Eq
 
@@ -246,3 +266,59 @@ txcbFinal txcb = txcbFinal_ txcb == maxBound
 -- only builds coinbases that are final and unlocked
 txCoinBase :: Word32 -> Script -> NEList TxOutput -> TxCoinBase
 txCoinBase v en os = TxCoinBase v en maxBound os unlocked
+
+-- This should be split into a block header (probably with cached merkle root) and transactions.
+data Block = Block { bVersion :: Word32 -- unused
+                   , bPrevBlock :: Hash
+                   , bTimestamp_ :: Word32
+                   , bBits :: Difficulty
+                   , bNonce :: Word32
+                   , bCoinBase :: TxCoinBase
+                   , bTxs :: [Tx]
+                   }
+
+instance Serialize Block where
+  get = do
+         v <- getWord32le
+         pb <- get
+         get :: Get Hash -- ignore the serialized merkle root; we will compute it ourselves
+         t <- getWord32le
+         b <- get
+         n <- getWord32le
+         len <- getVarInteger
+         unless (len < toInteger (maxBound :: Int))
+                (fail $ "get (Block) transaction list too long")
+         (cb,txs) <- if len == 0 then fail "get (Block): found only block header"
+                                 else (,) <$> get <*> replicateM (fromInteger len - 1) get
+         makeBlock v pb t b n cb txs
+
+  put bl@(Block v pb t b n cb txs) =
+    putWord32le v >> put pb >> put (bMerkle_root bl) >>
+    putWord32le t >> put b >> putWord32le n >>
+    putVarInteger (toInteger (length txs) + 1) >> put cb >> mapM_ put txs
+
+bTimestamp :: Block -> UTCTime
+bTimestamp = posixSecondsToUTCTime . fromIntegral . bTimestamp_
+
+bHash :: Block -> Hash
+bHash bl@(Block v pb t b n _ _) =
+  hashBS . runPut $ putWord32le v >> put pb >> put (bMerkle_root bl) >>
+                    putWord32le t >> put b >> putWord32le n
+
+bMerkle_root :: Block -> Hash
+bMerkle_root b = merkleHash $ hash (bCoinBase b) <| map hash (bTxs b)
+
+block :: Word32 -> Hash -> UTCTime -> Difficulty -> Word32 -> TxCoinBase -> [Tx] -> Maybe Block
+block v pb t' b n cb txs = do
+  guard ((0 <= t) && (t <= toInteger (maxBound :: Word32)))
+  makeBlock v pb (fromInteger t) b n cb txs
+ where
+  t :: Integer
+  t = round . utcTimeToPOSIXSeconds $ t'
+
+makeBlock :: (Monad m) => Word32 -> Hash -> Word32 -> Difficulty -> Word32 -> TxCoinBase -> [Tx] ->
+                          m Block
+makeBlock v pb t b n cb txs = unless (hashMeetsTarget (bHash blk) b) (fail $ "block : Target not met")
+                           >> return blk
+ where
+  blk = Block v pb t b n cb txs
