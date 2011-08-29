64e38cc42c5d933738b1316c3a0ba435f54ec3cc
Russell O'Connor <roconnor@theorem.ca>
2011-08-29 02:15:29 +0200

    CoinMap
    Ignore-this: 857c019a60355c4ff0b99c765c97e7fb
    
    darcs-hash:20110829001529-e9b40-e814d4fcca2da1291bb227ed2d16ec20c02c866c

--- Purecoin/Core/Transaction.hs
+++ Purecoin/Core/Transaction.hs
@@ -1,32 +1,95 @@
 module Purecoin.Core.Transaction
-       ( MakeHash, getTxInputs
+       ( Coins, CoinMap, emptyCoinMap, coinMapSize, addCoins, addTransaction, prepcbTransaction
        ) where
 
 import Data.Bits (testBit, (.&.))
 import Data.Word (Word8)
-import Data.Maybe (fromMaybe)
+import Data.Maybe (fromMaybe, maybeToList)
+import Data.Monoid (mconcat, mappend)
+import qualified Data.Map as M
 import Control.Applicative ((<$>))
+import Control.Monad (unless, zipWithM)
+import qualified Control.Monad.State as SM
 import Data.NEList (NEList(..), (<|), appendNE, toList)
 import Data.ByteString (append)
 import Purecoin.Utils (integerByteStringBE)
 import Purecoin.Core.Serialize (encode, runPut, put, putWord32le)
-import Purecoin.Core.Script (OP, opsScript)
-import Purecoin.Core.Hash (Hash, hashBS)
+import Purecoin.Core.Script (OP, MakeHash, scriptOps, opsScript, doScripts, execScriptMonad)
+import Purecoin.Core.Hash (Hash, hash, hashBS)
 import Purecoin.Core.DataTypes ( TxInput(..)
                                , GeneralizedTx(..), Tx
-                               , nullOutput
+                               , TxCoinBase, txcbOut
+                               , TxOutput, nullOutput, txoValue, txoScript
+                               , OutPoint, outPoint
+                               , BTC
                                )
 import Purecoin.Core.Signature ( HashKind(..), CoinSignature
                                , csHashKind, csAnyoneCanPay, csHashType)
 
-type MakeHash = [[OP]] -> CoinSignature -> Integer
+data Coins = Coins [(OutPoint, TxOutput)]
+
+coins :: Hash -> [TxOutput] -> Maybe Coins
+coins h txos = Coins <$> zipWithM f txos [0..]
+ where
+  f txo i = do op <- outPoint h i
+               return (op, txo)
+
+newtype CoinMap = CoinMap (M.Map OutPoint TxOutput)
+
+emptyCoinMap :: CoinMap
+emptyCoinMap = CoinMap M.empty
+
+coinMapSize (CoinMap m) = M.size m
+
+removeCoin :: (Monad m) => OutPoint -> SM.StateT CoinMap m TxOutput
+removeCoin key = do (CoinMap cm) <- SM.get
+                    txo <- maybe (fail errMsg) return $ M.lookup key cm
+                    SM.put . CoinMap . M.delete key $ cm
+                    return txo
+ where
+  errMsg = "Input "++show key++" not found"
+
+addCoins (Coins toInsert) = SM.modify $ \(CoinMap cm) -> CoinMap $ foldr (uncurry M.insert) cm toInsert
+
+addTransaction :: Tx -> SM.StateT CoinMap (Either String) (BTC,BTC)
+addTransaction tx =
+  do valueInputs <- mapM validateTxi . getTxInputs $ tx
+     let ins = mconcat valueInputs
+     let outs = mconcat valueOutputs
+     unless (outs <= ins)
+            (fail $ "Outputs greater than inputs in Tx: "++show txHash++
+                    " outs:" ++ show outs ++ " ins:" ++ show ins)
+     cs <- maybe (fail "addTransaction: bad coins!!") return $ coins (hash tx) txos
+     addCoins cs
+     return (ins, outs)
+  where
+   txHash = hash tx
+   txos = toList . txOut $ tx
+   valueOutputs = map txoValue txos
+   validateTxi (txi, mkHash) = do
+     ptxo <- removeCoin . txiPreviousOutput $ txi
+     opsSig <- either fail return $ scriptOps (txiScript txi)
+     opsCheck <- either fail return $ scriptOps (txoScript ptxo)
+     maybe (fail errMsg) return . execScriptMonad mkHash $ doScripts (toList opsSig)
+                                                        >> doScripts (toList opsCheck)
+     return (txoValue ptxo)
+    where
+     errMsg = "Script for "++show (txiPreviousOutput txi)++" in transaction "++show txHash++" failed"
+
+prepcbTransaction :: (BTC,BTC) -> TxCoinBase -> Either String Coins
+prepcbTransaction (ins, otherOuts) tx
+  | outs <= ins = maybe (fail "prebcbTransaction: bad coins!!") return $ coins (hash tx) txos
+  | otherwise   = fail $ "Outputs greater than inputs in coinbase Tx: "++show txHash++
+                         " outs:" ++ show outs ++ " ins:" ++ show ins
+ where
+  txHash = hash tx
+  txos = toList . txcbOut $ tx
+  outs = mconcat (map txoValue txos) `mappend` otherOuts
+  toInsert = (hash tx, toList (txcbOut tx))
 
 getTxInputs :: Tx -> [(TxInput, MakeHash)]
 getTxInputs tx = map result . selections . toList . txIn $ tx
   where
-   selections :: [a] -> [([a],a,[a])]
-   selections [] = []
-   selections (x:xs) = ([],x,xs):(map (\(l,m,r) -> (x:l,m,r)) (selections xs))
    result (l, m, r) = (m, makeHash l m r)
    setScript s txi = txi{txiScript = opsScript (NENil s)}
    makeHash l m r script sig = fromMaybe 1 $ do
@@ -55,3 +118,7 @@ getTxInputs tx = map result . selections . toList . txIn $ tx
         zipOutput _      []       = fail "getTxInputs: SIGHASH_SINGLE out of range"
         zipOutput []     (out:_)  = return [out]
         zipOutput (_:ls) (_:outs) = (nullOutput:) <$> zipOutput ls outs
+
+selections :: [a] -> [([a],a,[a])]
+selections [] = []
+selections (x:xs) = ([],x,xs):(map (\(l,m,r) -> (x:l,m,r)) (selections xs))
